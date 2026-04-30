d1732b2c58d644ecef51db2bb600e4a54d31fac8
chudini-creator <igorczew52@gmail.com>
2026-04-30 21:29:09 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import '@fontsource/poppins/400.css';
 import '@fontsource/poppins/500.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import React, { Suspense, lazy, useEffect } from 'react';
+import { HelmetProvider } from 'react-helmet-async';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import LoadingScreen from "./components/LoadingScreen";
@@ -36,35 +37,37 @@ function App() {
 
     return () => elementsToAnimate.forEach(el => observer.unobserve(el));
   }, []);
-  return(
-  <BrowserRouter>
-      <Header />
-      <Suspense
-        fallback={
-          <LoadingScreen
-            title="Ładowanie strony"
-            description="Przy słabszym internecie może to potrwać chwilę."
-          />
-        }
-      >
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/sklepy" element={<Sklepy />} />
-          <Route path="/pierogarnia" element={<Pierogarnia />} />
-          <Route path="/grupy-zorganizowane" element={<Grupy />} />
-          <Route path="/pokoje" element={<Pokoje />} />
-          <Route path="/apartamenty" element={<Apartamenty />} />
-          <Route path="/strefa-relaksu" element={<StrefaRelaksu />} />
-          <Route path="/współpraca" element={<Wspolpraca />} />
-          <Route path="/śniadania" element={<Breakfest />} />
-          <Route path="/weekendy-tematyczne" element={<Weekendy />} />
-          <Route path="/winnica-zamojska" element={<WinnicaZamojska />} />
-          <Route path="/pole-namiotowe" element={<PoleNamiotowe />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </Suspense>
-      <Footer />
-    </BrowserRouter>
+  return (
+    <HelmetProvider>
+      <BrowserRouter>
+        <Header />
+        <Suspense
+          fallback={
+            <LoadingScreen
+              title="Ładowanie strony"
+              description="Przy słabszym internecie może to potrwać chwilę."
+            />
+          }
+        >
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sklepy" element={<Sklepy />} />
+            <Route path="/pierogarnia" element={<Pierogarnia />} />
+            <Route path="/grupy-zorganizowane" element={<Grupy />} />
+            <Route path="/pokoje" element={<Pokoje />} />
+            <Route path="/apartamenty" element={<Apartamenty />} />
+            <Route path="/strefa-relaksu" element={<StrefaRelaksu />} />
+            <Route path="/wspolpraca" element={<Wspolpraca />} />
+            <Route path="/sniadania" element={<Breakfest />} />
+            <Route path="/weekendy-tematyczne" element={<Weekendy />} />
+            <Route path="/winnica-zamojska" element={<WinnicaZamojska />} />
+            <Route path="/pole-namiotowe" element={<PoleNamiotowe />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
+        <Footer />
+      </BrowserRouter>
+    </HelmetProvider>
   )
 }
 
