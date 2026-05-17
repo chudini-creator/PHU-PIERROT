1c5a9b5e62ed620ee918e7aa9af27630d2862860
chudini-creator <igorczew52@gmail.com>
2026-05-17 11:54:02 +0200

    Update WinnicaZamojska.jsx

--- src/pages/WinnicaZamojska.jsx
+++ src/pages/WinnicaZamojska.jsx
@@ -15,7 +15,8 @@ function WinnicaZamojska() {
                 <meta property="og:type" content="website" />
                 <meta property="og:url" content="https://phupierrot.pl/winnica-zamojska" />
                 <meta property="og:title" content="Winnica Zamojska & Degustacje | Dwór Ujazdowski Lubelskie" />
-                <meta property="og:description" content="Odkryj smaki regionu z Winnicą Zamojską w Dworze Ujazdowskim. Degustacje win i lokalnych produktów w sercu Lubelszczyzny." />
+                <meta property="og:description" content="Odkryj smaki regionu z Winnicą Zamojską. Degustacje win i lokalnych produktów w sercu Lubelszczyzny." />
+                <meta property="og:image" content="https://phupierrot.pl/img/Degustacja.webp" />
             </Helmet>
             <Hero title="Winnica Zamojska" bgImage="/img/Wino.mp4" nextID="#WinnicaIntro"/>
             <Intro id="WinnicaIntro" title="Weekend z winem" text="Zapraszamy na wyjątkowy weekend poświęcony winu. Czeka na Państwa degustacja starannie wyselekcjonowanych trunków z lokalnej winnicy oraz zwiedzanie jej terenu. Całość odbywa się w kameralnej atmosferze, w otoczeniu natury — idealna okazja, by oderwać się od codzienności i zanurzyć w świecie smaków i aromatów."/>
